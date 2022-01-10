a9b39431dbcc7663c6bf255b8fb8d0c1e2e70f63
Yves-Oliveira <yvesdeoliveiraribeiro@gmail.com>
2022-01-09 21:35:29 -0300

    Ajustes

--- banking/src/main/java/com/blindbank/Banking/controller/ContaController.java
+++ banking/src/main/java/com/blindbank/Banking/controller/ContaController.java
@@ -5,6 +5,7 @@ import com.blindbank.Banking.model.Conta;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,13 +16,13 @@ public class ContaController {
   @Autowired
   private ContaDao dao;
 
-        @GetMapping("/contas")
-        public List<Conta> recuperarTodas() {
-          return (List<Conta>) dao.findAll();
-        }
+  @GetMapping("/contas")
+  public List<Conta> recuperarTodas() {
+    return (List<Conta>) dao.findAll();
+  }
 
-        @GetMapping("/contas/{numero}")
-        public Conta recuperarPeloNumero(@PathVariable int numero) {
-          return dao.findById(numero).orElse(null);
-        }
+  @GetMapping("/contas/{numero}")
+  public Conta recuperarPeloNumero(@PathVariable int numero) {
+    return dao.findById(numero).orElse(null);
+  }
 }
